a348271549b33771c9b85c880563ac305c174867
Yujeemin <mo_om1012@naver.com>
2017-05-29 22:57:01 +0900

    [COMMENT]Add comments for class CalculationHelper

--- app/src/main/java/com/fallingstar/yorimi/Helper/Calculation/CalculationHelper.java
+++ app/src/main/java/com/fallingstar/yorimi/Helper/Calculation/CalculationHelper.java
@@ -10,12 +10,17 @@ import java.util.ArrayList;
  */
 
 public class CalculationHelper {
-
+    /*
+    Define variables.
+     */
     private static long[] startMillis;
     private static boolean isMade = false;
     private static DatabaseHelper db;
     private static int size;
-
+    /*
+    purpose : Constructor of CalculationHelper.
+    If the db instance was already made, initialization is blocked, So the every class can use only one static instance with a memory saving
+     */
     public CalculationHelper(){
         if (!isMade){
             db = MainActivity.getYoribi();
@@ -24,7 +29,9 @@ public class CalculationHelper {
             isMade = true;
         }
     }
-
+    /*
+    purpose : Class that help to set milliseconds of each alarms
+     */
     public void setStartMillis(long _millis, int idx){
         /* save previous data */
         int i;
@@ -41,16 +48,28 @@ public class CalculationHelper {
             startMillis[i] = tempMillis[i];
         }
     }
+    /*
+      purpose : Class that help to get milliseconds of alarms.
+       */
     public long getStartMillis(int idx){
         return startMillis[idx];
     }
+    /*
+   purpose : Class that help to calculate cost per minutes.
+    */
     public double getCostPerMinute(int time, int cost){
         return (double) cost/time;
     }
+    /*
+  purpose : Class that set current time and help to calculate difference between two times
+   */
     public long getDiffFromPrevTimeWithMilliSec(long prevTime, int idx){
         long currentTime = startMillis[idx];
         return getDiffOfTwoMilliSec(prevTime, currentTime);
     }
+    /*
+    purpose : Class that help to change milliseconds to seconds,minutes,hours.
+     */
     public int getMinuteFromMilliSec(long milliSec){
         int sec,min,hour;
         float totalMinD = 0;
@@ -65,6 +84,9 @@ public class CalculationHelper {
 
         return totalMinI;
     }
+    /*
+    purpose : Class that help to calculate difference between current time and previous time.
+     */
     private long getDiffOfTwoMilliSec(long prevTime, long currentTime){
         return prevTime-currentTime;
     }
